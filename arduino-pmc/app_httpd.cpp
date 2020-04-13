4ba89f9d302d0b419ed03107078bcd6353a22860
alxan <aririneo@hotmail.com>
2020-04-13 21:14:03 +0800

    Jacobski (#1)
    * ip at 192.168.1.1 and stream put in captive portal default page
    
    * frontend page groomed with inpupt interface for setting title
    
    * saves title to eeprom
    
    * setting title. modified cam view to portrait
    
    Co-authored-by: jacobski <ejr.jacob@gmail.com>

--- arduino-pmc/app_httpd.cpp
+++ arduino-pmc/app_httpd.cpp
@@ -21,6 +21,8 @@
 #include "fb_gfx.h"
 #include "fd_forward.h"
 #include "fr_forward.h"
+#include <EEPROM.h>
+
 
 #define ENROLL_CONFIRM_TIMES 5
 #define FACE_ID_SAVE_NUMBER 7
@@ -62,6 +64,36 @@ static int8_t recognition_enabled = 0;
 static int8_t is_enrolling = 0;
 static face_id_list id_list = {0};
 
+void storeString(char add,String data)
+{
+  int _size = data.length();
+  int i;
+  for(i=0;i<_size;i++)
+  {
+    EEPROM.write(add+i,data[i]);
+  }
+  EEPROM.write(add+_size,'\0');   //Add termination null character for String Data
+  EEPROM.commit();
+}
+ 
+ 
+String fetchString(char add)
+{
+  int i;
+  char data[100]; //Max 100 Bytes
+  int len=0;
+  unsigned char k;
+  k=EEPROM.read(add);
+  while(k != '\0' && len<500)   //Read until null character
+  {    
+    k=EEPROM.read(add+len);
+    data[len]=k;
+    len++;
+  }
+  data[len]='\0';
+  return String(data);
+}
+ 
 static ra_filter_t * ra_filter_init(ra_filter_t * filter, size_t sample_size){
     memset(filter, 0, sizeof(ra_filter_t));
 
@@ -215,94 +247,59 @@ static size_t jpg_encode_stream(void * arg, size_t index, const void* data, size
     return len;
 }
 
-static esp_err_t capture_handler(httpd_req_t *req){
-    camera_fb_t * fb = NULL;
+static esp_err_t update_handler(httpd_req_t *req) {
+    Serial.println("updating title.............");
     esp_err_t res = ESP_OK;
-    int64_t fr_start = esp_timer_get_time();
+    
 
-    fb = esp_camera_fb_get();
-    if (!fb) {
-        Serial.println("Camera capture failed");
-        httpd_resp_send_500(req);
-        return ESP_FAIL;
-    }
+    char*  buf;
+    size_t buf_len;
+    char variable[32] = {0,};
+    char value[32] = {0,};
 
-    httpd_resp_set_type(req, "image/jpeg");
-    httpd_resp_set_hdr(req, "Content-Disposition", "inline; filename=capture.jpg");
-    httpd_resp_set_hdr(req, "Access-Control-Allow-Origin", "*");
-
-    size_t out_len, out_width, out_height;
-    uint8_t * out_buf;
-    bool s;
-    bool detected = false;
-    int face_id = 0;
-    if(!detection_enabled || fb->width > 400){
-        size_t fb_len = 0;
-        if(fb->format == PIXFORMAT_JPEG){
-            fb_len = fb->len;
-            res = httpd_resp_send(req, (const char *)fb->buf, fb->len);
+    buf_len = httpd_req_get_url_query_len(req) + 1;
+    if (buf_len > 1) {
+        buf = (char*)malloc(buf_len);
+        if(!buf){
+            httpd_resp_send_500(req);
+            return ESP_FAIL;
+        }
+        if (httpd_req_get_url_query_str(req, buf, buf_len) == ESP_OK) {
+            if (httpd_query_key_value(buf, "var", variable, sizeof(variable)) == ESP_OK &&
+                httpd_query_key_value(buf, "val", value, sizeof(value)) == ESP_OK) {
+            } else {
+                free(buf);
+                httpd_resp_send_404(req);
+                return ESP_FAIL;
+            }
         } else {
-            jpg_chunking_t jchunk = {req, 0};
-            res = frame2jpg_cb(fb, 80, jpg_encode_stream, &jchunk)?ESP_OK:ESP_FAIL;
-            httpd_resp_send_chunk(req, NULL, 0);
-            fb_len = jchunk.len;
+            free(buf);
+            httpd_resp_send_404(req);
+            return ESP_FAIL;
         }
-        esp_camera_fb_return(fb);
-        int64_t fr_end = esp_timer_get_time();
-        Serial.printf("JPG: %uB %ums\n", (uint32_t)(fb_len), (uint32_t)((fr_end - fr_start)/1000));
-        return res;
-    }
-
-    dl_matrix3du_t *image_matrix = dl_matrix3du_alloc(1, fb->width, fb->height, 3);
-    if (!image_matrix) {
-        esp_camera_fb_return(fb);
-        Serial.println("dl_matrix3du_alloc failed");
-        httpd_resp_send_500(req);
+        free(buf);
+    } else {
+        httpd_resp_send_404(req);
         return ESP_FAIL;
     }
+   
 
-    out_buf = image_matrix->item;
-    out_len = fb->width * fb->height * 3;
-    out_width = fb->width;
-    out_height = fb->height;
+    if(!strcmp(variable, "title")) {
+        Serial.println("updating title...");
+        Serial.println(value);
 
-    s = fmt2rgb888(fb->buf, fb->len, fb->format, out_buf);
-    esp_camera_fb_return(fb);
-    if(!s){
-        dl_matrix3du_free(image_matrix);
-        Serial.println("to rgb888 failed");
-        httpd_resp_send_500(req);
-        return ESP_FAIL;
+        String retrievedString = fetchString(0);
+        Serial.print("The String we read from EEPROM: ");
+        Serial.println(retrievedString);
+
+        storeString(0, value);
     }
-
-    box_array_t *net_boxes = face_detect(image_matrix, &mtmn_config);
-
-    if (net_boxes){
-        detected = true;
-        if(recognition_enabled){
-            face_id = run_face_recognition(image_matrix, net_boxes);
-        }
-        draw_face_boxes(image_matrix, net_boxes, face_id);
-        free(net_boxes->score);
-        free(net_boxes->box);
-        free(net_boxes->landmark);
-        free(net_boxes);
-    }
-
-    jpg_chunking_t jchunk = {req, 0};
-    s = fmt2jpg_cb(out_buf, out_len, out_width, out_height, PIXFORMAT_RGB888, 90, jpg_encode_stream, &jchunk);
-    dl_matrix3du_free(image_matrix);
-    if(!s){
-        Serial.println("JPEG compression failed");
-        return ESP_FAIL;
-    }
-
-    int64_t fr_end = esp_timer_get_time();
-    Serial.printf("FACE: %uB %ums %s%d\n", (uint32_t)(jchunk.len), (uint32_t)((fr_end - fr_start)/1000), detected?"DETECTED ":"", face_id);
     return res;
 }
 
 static esp_err_t stream_handler(httpd_req_t *req){
+    Serial.println("streaming.............");
+  
     camera_fb_t * fb = NULL;
     esp_err_t res = ESP_OK;
     size_t _jpg_buf_len = 0;
@@ -612,10 +609,10 @@ void startCameraServer(){
         .user_ctx  = NULL
     };
 
-    httpd_uri_t capture_uri = {
-        .uri       = "/capture",
+    httpd_uri_t update_uri = {
+        .uri       = "/update",
         .method    = HTTP_GET,
-        .handler   = capture_handler,
+        .handler   = update_handler,
         .user_ctx  = NULL
     };
 
@@ -643,18 +640,17 @@ void startCameraServer(){
     mtmn_config.o_threshold.nms = 0.7;
     mtmn_config.o_threshold.candidate_number = 1;
     
-    face_id_init(&id_list, FACE_ID_SAVE_NUMBER, ENROLL_CONFIRM_TIMES);
-    
+    config.server_port = 81;
     Serial.printf("Starting web server on port: '%d'\n", config.server_port);
     if (httpd_start(&camera_httpd, &config) == ESP_OK) {
         httpd_register_uri_handler(camera_httpd, &index_uri);
         httpd_register_uri_handler(camera_httpd, &cmd_uri);
         httpd_register_uri_handler(camera_httpd, &status_uri);
-        httpd_register_uri_handler(camera_httpd, &capture_uri);
+        httpd_register_uri_handler(camera_httpd, &update_uri);
     }
 
-    config.server_port += 1;
-    config.ctrl_port += 1;
+    config.server_port = 82;
+    config.ctrl_port = 83;
     Serial.printf("Starting stream server on port: '%d'\n", config.server_port);
     if (httpd_start(&stream_httpd, &config) == ESP_OK) {
         httpd_register_uri_handler(stream_httpd, &stream_uri);
--- arduino-pmc/arduino-pmc.ino
+++ arduino-pmc/arduino-pmc.ino
@@ -1,6 +1,7 @@
-#include "esp_camera.h"
+#include "esp_camera.h" 
 #include <WiFi.h>
 #include <DNSServer.h>
+#include <EEPROM.h>
 
 //
 // WARNING!!! Make sure that you have either selected ESP32 Wrover Module,
@@ -17,16 +18,46 @@
 #include "camera_pins.h"
 
 const byte DNS_PORT = 53;
-IPAddress apIP(172, 217, 28, 1);
+IPAddress apIP(192, 168, 1, 1);
 DNSServer dnsServer;
 WiFiServer server(80);
 
-String responseHTML = ""
-  "<!DOCTYPE html><html><head><title>CaptivePortal</title></head><body>"
-  "<meta http-equiv=\"refresh\" content=\"1;url=http://172.217.28.1:81/stream\"/>"
-  "<h1>Please Wait!</h1><p>You will be redirected to the camera stream "
-  "shortly.</p></body></html>";
+String getPortalResponse() {
+  String responseHTML = ""
+  "<!DOCTYPE html><html><head><title>Patient Monitoring</title>"
+  "<style>  body {width: 100%; overflow-x: hidden;font-family: Arial;  }  button, input {    font-size: 18pt;  }    #cam-title {    font-size: 20pt;  }  #cam-title input {    border-style: dotted;    border-width: 0;    width: 100%;  }    #title-edit {    display: none;  }</style>"
+  "</head><body>"
+  "<div style='text-align: center;'>  <h1 style='display: inline;'>Patient Monitoring Camera 2020</h1>  <h5 style='display: inline;'>by #lockdownlab</h5>  <p style='margin:0; font-size: 12pt;'>Open in browser: http://192.168.1.1</p>  </div>  <br>  "
+  "<div id='title-display'><div style='float: left; padding: 5px;'><div>Camera Name:</div></div>    <div style='float: right;'><button onClick='onClickEdit();'>edit</button></div>    <div id='cam-title' style='border-style: solid; overflow: hidden;'>Room Name</div>  </div>"
+  "<div id='title-edit'><div style='float: left; padding: 5px;'><div>Camera Name:</div></div>    <div style='float: right;'><button onClick='onClickSave();'>save</button></div>    <div id='cam-title' style='border-style: dotted; overflow: hidden;'><input id='title-input' type='text' value=''></div>  </div>"
+  "<div style='width: 100%;'>    <img style='width:130%; transform: rotate(90deg) translateX(15%) translateY(16%);'  src='http://192.168.1.1:82/stream'/>  </div>"
+  "<script>"
+  "document.getElementById('cam-title').innerHTML = unescape('" + mainFetchString(0) + "');"
+  "function onClickEdit() {    document.getElementById('title-display').style.display = 'none';    document.getElementById('title-edit').style.display = 'block';    var oldTitle = document.getElementById('cam-title').innerHTML;    document.getElementById('title-input').value = oldTitle;  }"
+  "function onClickSave() {    document.getElementById('title-edit').style.display = 'none';    document.getElementById('title-display').style.display = 'block';    var newTitle = document.getElementById('title-input').value;    document.getElementById('cam-title').innerHTML = newTitle;        const Http = new XMLHttpRequest();    const url='http://192.168.1.1:81/update?var=title&val='+newTitle+'&ts='+Date.now();    Http.open('GET', url);    Http.send();  }"
+  "</script></body></html>";
+  return responseHTML;  
+}
+
 void startCameraServer();
+
+
+String mainFetchString(char add)
+{
+  int i;
+  char data[100]; //Max 100 Bytes
+  int len=0;
+  unsigned char k;
+  k=EEPROM.read(add);
+  while(k != '\0' && len<500)   //Read until null character
+  {    
+    k=EEPROM.read(add+len);
+    data[len]=k;
+    len++;
+  }
+  data[len]='\0';
+  return String(data);
+}
 
 void setup() {
   Serial.begin(115200);
@@ -55,7 +86,7 @@ void setup() {
   config.xclk_freq_hz = 20000000;
   config.pixel_format = PIXFORMAT_JPEG;
   //init with high specs to pre-allocate larger buffers
-  if(psramFound()){
+  if (psramFound()) {
     config.frame_size = FRAMESIZE_UXGA;
     config.jpeg_quality = 10;
     config.fb_count = 2;
@@ -66,7 +97,7 @@ void setup() {
   }
 
   // camera init
-  
+
   esp_err_t err = esp_camera_init(&config);
   if (err != ESP_OK) {
     Serial.printf("Camera init failed with error 0x%x", err);
@@ -81,17 +112,25 @@ void setup() {
   delay(100);
   WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 0, 0));
   WiFi.softAP("Patient Monitoring Camera");
-  
+
   // if DNSServer is started with "*" for domain name, it will reply with
   // provided IP to all DNS request
   dnsServer.start(DNS_PORT, "*", apIP);
 
-server.begin();
+  server.begin();
 
 
   startCameraServer();
 
+  EEPROM.begin(512);
+
+
+
+  String retrievedString = mainFetchString(0);
+  Serial.print("The String we read from EEPROM: ");
+  Serial.println(retrievedString);
 }
+
 
 void loop() {
   dnsServer.processNextRequest();
@@ -107,7 +146,8 @@ void loop() {
             client.println("HTTP/1.1 200 OK");
             client.println("Content-type:text/html");
             client.println();
-            client.print(responseHTML);
+            client.print(getPortalResponse());
+
             break;
           } else {
             currentLine = "";
@@ -117,6 +157,6 @@ void loop() {
         }
       }
     }
-   // client.stop();  
-  }   
+    // client.stop();
+  }
 }
